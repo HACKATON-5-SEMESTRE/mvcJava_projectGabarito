c3bbfef3de9e934112849849909afaf3e9128f96
Daniel Oliveira <danielmo352@gmail.com>
2025-06-17 22:11:14 -0300

    Estruturando Projeto

---
+++ src/main/java/Hackaton_5_Semestre/projectGabarito/ProjectGabaritoApplication.java
@@ -0,0 +1,13 @@
+package Hackaton_5_Semestre.projectGabarito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGabaritoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGabaritoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Hackaton_5_Semestre/projectGabarito/ProjectGabaritoApplicationTests.java
@@ -0,0 +1,13 @@
+package Hackaton_5_Semestre.projectGabarito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGabaritoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
