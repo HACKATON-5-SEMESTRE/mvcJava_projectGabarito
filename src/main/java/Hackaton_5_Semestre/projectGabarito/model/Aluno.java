5a2971f3d64a6495b9171eafb86833742ba94686
Igor <igorantonucci5@gmail.com>
2025-06-19 15:16:28 -0300

    Criação do CRUD de alunos.

---
+++ src/main/java/Hackaton_5_Semestre/projectGabarito/model/Aluno.java
@@ -0,0 +1,26 @@
+package Hackaton_5_Semestre.projectGabarito.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Aluno extends Usuario{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "id")
+    private Usuario usuario;
+
+    private Long RA;
+}
